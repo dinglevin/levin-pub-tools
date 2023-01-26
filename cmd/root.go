53882dcc7c942c07bf916ff6ef866aed1d10d692
dinglevin <dingweiqiong@gmail.com>
2023-01-26 23:03:42 +0800

    Hello world实现

---
+++ cmd/root.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2023 dinglevin <dinglevin@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "chgmd5",
+	Short: "A small tool to change a file's MD5 hash",
+	Long:  `A small tool to change a file's MD5 hash`,
+	Run: func(cmd *cobra.Command, args []string) {
+		appendStr, err := cmd.Flags().GetString("append")
+		if err != nil {
+			fmt.Println("Invalid append flags: ", err)
+			return
+		}
+
+		fmt.Println("Hello world!", appendStr)
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringP("append", "a", "@@LEVIN@@", "Append string to the file")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 dinglevin <dinglevin@gmail.com>
+*/
+package main
+
+import "github.com/dinglevin/chgmd5/cmd"
+
+func main() {
+	cmd.Execute()
+}
